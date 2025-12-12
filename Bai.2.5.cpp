2ef360713cb763bc25434afaf17ab29ba87ef933
HoangLeNgoc08 <lengochoang.qn@gmail.com>
2025-12-12 23:42:30 +0700

    Loop

--- Bai.2.5.cpp
+++ Bai.2.5.cpp
@@ -2,17 +2,22 @@
 
 int main()
 {
+	/* declare and read the number of rows 'n' from user input */
 	int n;
 	scanf("%d", &n);
 	
+	/* loop */
 	for(int i = 0 ; i < n ; i++)
 	{
+		/* So dau tien la so 1 */
+		
 		int tmp = 1;
 		for (int j = 0 ; j <= i ; j++)
 		{
 			printf("%d ", tmp);
 			tmp = tmp * (i - j) / (j + 1);
 		}
+		/* Di chuyen xuong dong khac*/
 		printf("\n");
 	}
 	
